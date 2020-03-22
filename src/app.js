139309f446d1dbf764046a5e83683e3318bc1168
puranmgr <puranmgr@gmail.com>
2020-03-22 17:19:17 +0000

    getting data from the api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 import React from "react";
+import Axios from "axios";
 
 export default class App extends React.Component {
+
+    state = {
+        confirmed: 0,
+        recovered: 0,
+        deaths: 0
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    async getData() {
+        const res = await Axios.get("https://covid19.mathdro.id/api");
+        this.setState({
+            confirmed: res.data.confirmed.value,
+            recovered: res.data.recovered.value,
+            deaths: res.data.deaths.value
+        })
+    }
+
     render() {
         return (<div>App</div>);
     }
